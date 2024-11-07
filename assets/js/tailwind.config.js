b01b1180576f7da85d5816f99be7b10e69730790
OumaymaBrd <oumaymalovemaths>
2024-11-07 22:14:08 +0100

    Ajout // tailwind.config.js pour la configuration de responsiviter de button carroussels

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    theme: {
+      extend: {
+        spacing: {
+          '180px': '180px', // Pour `top` sur grands écrans
+          '316px': '316px', // Pour `left` sur grands écrans
+          '150px': '150px', // Pour `top` sur écrans moyens
+          '250px': '250px', // Pour `left` sur écrans moyens
+          '100px': '100px', // Pour `top` sur petits écrans
+          '200px': '200px', // Pour `left` sur petits écrans
+        },
+      },
+    },
+    plugins: [],
+  }
