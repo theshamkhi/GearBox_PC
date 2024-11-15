dceca510a3b57e368dd8d9a246b7e37005224a0c
Hichamelhmaydi <ct1594065@gmail.com>
2024-11-15 11:46:50 +0100

    added table and added responsive

--- assets/GearBox.js
+++ assets/GearBox.js
@@ -1,4 +1,19 @@
-document.addEventListener('DOMContentLoaded', () => {
+/* Page Devis */
+document.addEventListener('DOMContentLoaded',() => {
+    const boutton = document.getElementById('boutton');
+    let tbody = document.getElementById('tbd');
+        tbody.innerHTML=`
+        <tr>
+            <td>Produit1 - Description</td>
+            <td>3</td>
+            <td>150MAD</td>
+            <td>red</td>
+        </tr>
+        `
 
-    
-});
+    const a=document.getElementById('download');
+    a.addEventListener('click',funprint);
+    function funprint () {
+        window.print();
+    }
+})
