a0855cd29755c0f27de12b4bc822bf7f78475937
Oussama Benoujja <osama2code79@gmail.com>
2024-11-17 22:51:47 +0100

    Update GearBox.js

--- assets/GearBox.js
+++ assets/GearBox.js
@@ -1,6 +1,6 @@
 /* Page Accueil */
 
-if (window.location.pathname.endsWith("index.html")) {
+if (!window.location.pathname.endsWith("Catalogue.html")) {
     console.log("You are on catalogue.html");
 
 const slides = document.getElementById('carousel-images');
