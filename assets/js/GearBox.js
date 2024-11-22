01e5e33073455718fcacc49ae05bc86e75e38770
OumaymaBrd <oumaymalovemaths>
2024-11-22 11:39:16 +0100

    Add the data

--- assets/js/GearBox.js
+++ assets/js/GearBox.js
@@ -29,7 +29,7 @@ function fetchDataAndDisplay() {
         return;
     }
 
-    fetch('../data/data.json')
+    fetch('../data.json')
         .then(response => {
             if (!response.ok) {
                 throw new Error("Erreur lors du chargement du fichier data.json");
